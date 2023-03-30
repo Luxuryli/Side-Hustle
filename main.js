d50fb96b1bfde34c4adc2ffa0024f59357f86524
OLAKU TEMITOPE <olakutope4@gmail.com>
2023-03-30 02:05:25 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const form = document.querySelector('form');
+const thankYouMessage = document.querySelector('#submission');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+submission.classList.add('show');
+});
